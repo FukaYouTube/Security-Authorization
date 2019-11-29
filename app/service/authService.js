255e712559a5c140eb5011afc027b24bc1afbc79
Fuka <weendibol21300@gmail.com>
2019-11-29 16:18:35 +0600

    add const date

--- app/service/authService.js
+++ app/service/authService.js
@@ -1,7 +1,7 @@
-exports.loginPost = (req, res) => {
-    console.log(req.body)
+exports.loginPost = async (req, res) => {
+    const { username, password } = req.body
 }
 
-exports.registerPost = (req, res) => {
-    console.log(req.body)
+exports.registerPost = async (req, res) => {
+    const { username, first_name, email, password } = req.body
 }
