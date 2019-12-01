4b096e6cac4cb2081a47234c2a052e5d5febe7cb
Fuka <weendibol21300@gmail.com>
2019-12-01 19:09:32 +0600

    console log -> print

---
+++ source/print/print.js
@@ -0,0 +1,21 @@
+const { red, yellow, green, reset } = require('./colors')
+
+class Print {
+    constructor(message){
+        this.message = message
+    }
+
+    log(){
+        
+    }
+
+    warn(){
+
+    }
+
+    error(){
+
+    }
+}
+
+module.exports = Print
