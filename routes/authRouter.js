c3a32e504f351c24e3013c88e636b0d241b080ba
Fuka <weendibol21300@gmail.com>
2019-12-02 12:35:30 +0600

    router auth

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 
+router.get('/', (req, res) => res.render('authLogin'))
 
+router.get('/register', (req, res) => res.render('authRegister'))
 
 module.exports = router
