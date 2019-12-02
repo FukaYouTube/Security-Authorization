450f7a529df743149bea9dfb24c61df998276aae
Fuka <weendibol21300@gmail.com>
2019-12-02 12:08:36 +0600

    add, use pack, use, import routers

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
+// import express
 const express = require('express')
 const app = express()
 
+// import packages
+const expressSession    = require('express-session')
+const bodyParser        = require('body-parser')
 
+// use
+app.set('view engine', 'ejs')
+app.use('public', express.static('/public'))
+app.use(bodyParser.json(), bodyParser.urlencoded({ extended: false }))
+app.use(expressSession({ secret: process.env.SECRET, cookie: { maxAge: null }, resave: false, saveUninitialized: false }))
+
+// import routers
+const { authRouter } = require('./routes')
+
+// use routers
+app.use('/', authRouter)
 
 module.exports = app
