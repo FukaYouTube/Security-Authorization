81f9a7b86d9b9160b6fe6217edb41c4ee9f69008
Fuka <weendibol21300@gmail.com>
2019-12-04 14:19:07 +0600

    log undefined

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const { loginPost, registerPost } = require('../service/authService')
 
-router.get('/', (req, res) => res.render('authLogin'))
+router.get('/', (req, res) => res.render('authLogin', { log: undefined }))
 router.post('/', (req, res) => loginPost(req, res))
 
 router.get('/register', (req, res) => res.render('authRegister', { log: undefined }))
