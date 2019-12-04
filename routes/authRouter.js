9a3cf80c312fe4f414da3d83018ef851df1e12b5
Fuka <weendibol21300@gmail.com>
2019-12-04 13:40:36 +0600

    add post

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
+const { loginPost, registerPost } = require('../service/authService')
 
 router.get('/', (req, res) => res.render('authLogin'))
+router.post('/', (req, res) => loginPost(req, res))
 
-router.get('/register', (req, res) => res.render('authRegister'))
+router.get('/register', (req, res) => res.render('authRegister', { log: undefined }))
+router.post('/register', (req, res) => registerPost(req, res))
 
 module.exports = router
