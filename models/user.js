f14fb1d4e39703df418e0518e7aaa2d6de587e89
Fuka <weendibol21300@gmail.com>
2019-12-08 21:07:40 +0600

    update schema databases

--- models/user.js
+++ models/user.js
@@ -1,10 +1,12 @@
 const { Schema } = require('mongoose')
 
 let user = new Schema({
-    username:   String,
-    first_name: String,
-    email:      String,
-    password:   String,
+    username:   { type: String, required: true },
+    first_name: { type: String, required: true },
+    email:      { type: String, required: true },
+    password:   { type: String, required: true },
+
+    code:       Number,
 
     date:       { type: Date, default: Date.now }
 })
