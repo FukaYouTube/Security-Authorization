cd9543df503f1dc1a846126ec238ea357a3fc44e
Fuka <weendibol21300@gmail.com>
2019-12-08 21:07:54 +0600

    add actived code api

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
-const { loginPost, registerPost } = require('../service/authService')
+const { loginPost, registerPost, getApiRequestCode } = require('../service/authService')
 
 router.get('/', (req, res) => res.render('authLogin', { log: undefined }))
 router.post('/', (req, res) => loginPost(req, res))
 
 router.get('/register', (req, res) => res.render('authRegister', { log: undefined }))
 router.post('/register', (req, res) => registerPost(req, res))
+
+router.get('/code_actived/api/:userId&:code', (req, res) => getApiRequestCode(req, res))
 
 module.exports = router
