78c2ac10de41dd7163897816a7e43b847d9cf501
Fuka <weendibol21300@gmail.com>
2019-12-09 16:49:30 +0600

    add exports send to mail

--- service/mailService.js
+++ service/mailService.js
@@ -13,17 +13,22 @@ let mailer = nodemailer.createTransport({
 
 let msgJSON = JSON.parse(fs.readFileSync('source/messages.json', 'utf8'))
 
-exports.sendMail = (type = '', email, user_id, code) => {
-    switch(type){
-        case 'active/send code to email':
-            let urlCode = `https://localhost:8080/code_actived/api/${user_id}&${code}`
+exports.sendMailToCode = (email, user_id, code) => {
+        let urlCode = `https://localhost:8080/code_actived/api/${user_id}&${code}`
 
-            mailer.sendMail({
-                from: process.env.E_LOGIN,
-                to: email,
-                subject: msgJSON.snedCodeToEmail.subjects,
-                text: msgJSON.snedCodeToEmail.message + ' SecretCodeURL: ' + urlCode
-            }).catch((e) => print.error(e))
-        break
-    }
+        mailer.sendMail({
+            from: process.env.E_LOGIN,
+            to: email,
+            subject: msgJSON.snedCodeToEmail.subjects,
+            text: msgJSON.snedCodeToEmail.message + ' SecretCodeURL: ' + urlCode
+        }).catch((e) => print.error(e))
+}
+
+exports.sendMailToUnInput = (email, agent) => {
+    mailer.sendMail({
+        from: process.env.E_LOGIN,
+        to: email,
+        subject: msgJSON.unknownInput.subjects,
+        text: msgJSON.unknownInput.message + agent.toString()
+    }).catch((e) => print.error(e))
 }
